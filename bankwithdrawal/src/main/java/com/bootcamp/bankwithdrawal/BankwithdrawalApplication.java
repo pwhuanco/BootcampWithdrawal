61bf08bb387119eed4e5e382fb02ff76dfd39c71
pwhuanco <pedrohuanco@gmail.com>
2022-03-10 22:24:19 -0500

    primera subida

---
+++ bankwithdrawal/src/main/java/com/bootcamp/bankwithdrawal/BankwithdrawalApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankwithdrawal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankwithdrawalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankwithdrawalApplication.class, args);
+	}
+
+}
---
+++ bankwithdrawal/src/test/java/com/bootcamp/bankwithdrawal/BankwithdrawalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bankwithdrawal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankwithdrawalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
