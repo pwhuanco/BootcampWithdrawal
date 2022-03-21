3d80d886aa9d4c272f93d9262d6dd7b5a70d9113
pwhuanco <pedrohuanco@gmail.com>
2022-03-21 02:00:21 -0500

    checkstyle

--- bankwithdrawal/src/main/java/com/bootcamp/bankwithdrawal/util/AppUtils.java
+++ bankwithdrawal/src/main/java/com/bootcamp/bankwithdrawal/util/AppUtils.java
@@ -4,14 +4,17 @@ import com.bootcamp.bankwithdrawal.bean.Withdrawal;
 import com.bootcamp.bankwithdrawal.dto.WithdrawalDto;
 import org.springframework.beans.BeanUtils;
 
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class AppUtils {
 
+    @SuppressWarnings({"checkstyle:WhitespaceAfter", "checkstyle:WhitespaceAround", "checkstyle:FinalParameters", "checkstyle:MissingJavadocMethod"})
     public static WithdrawalDto entityToDto(Withdrawal withdrawal){
         WithdrawalDto accDto=new WithdrawalDto();
         BeanUtils.copyProperties(withdrawal,accDto);
         return accDto;
     }
 
+    @SuppressWarnings({"checkstyle:WhitespaceAfter", "checkstyle:WhitespaceAround", "checkstyle:FinalParameters", "checkstyle:MissingJavadocMethod"})
     public static Withdrawal dtoToEntity(WithdrawalDto accDto){
         Withdrawal withdrawal=new Withdrawal();
         BeanUtils.copyProperties(accDto,withdrawal);
